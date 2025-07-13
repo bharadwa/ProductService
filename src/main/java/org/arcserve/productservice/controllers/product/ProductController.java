00d3ddff825badb3755a4ee124ab6d59a9ac3d12
Bharadwaj <bharadwajreddy9@gmail.com>
2025-07-13 14:13:02 +0530

    fix product controller methods

--- src/main/java/org/arcserve/productservice/controllers/product/ProductController.java
+++ src/main/java/org/arcserve/productservice/controllers/product/ProductController.java
@@ -2,6 +2,7 @@ package org.arcserve.productservice.controllers.product;
 
 import org.arcserve.productservice.models.Product;
 import org.arcserve.productservice.services.product.ProductService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(@Qualifier("realProductService") ProductService productService) {
         this.productService = productService;
     }
 
